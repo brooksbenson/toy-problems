efd4964ea612948d757b1d4a587f70ec78d9476e
Brooks Benson <brooksbenson01@gmail.com>
2018-02-17 17:40:05 -0700

    progress on sumStrings

--- multiplying-numbers-as-strings/multiplying-numbers-as-strings.js
+++ multiplying-numbers-as-strings/multiplying-numbers-as-strings.js
@@ -1,11 +1,16 @@
 // should take two argument, each being string representations of numbers,
 // and return the product of multiplying those numbers as a string
+
+
 function add(a, b) {
-  a.length < b.length
-    ? a = a.padEnd(b.length, '0')
-    : b = b.padEnd(a.length, '0');
-  let res = [];
-  [...a].forEach((x, i) => {
-    
-  })
+  a = [...a.replace(/^0+/, '')].reverse(); 
+  b = [...b.replace(/^0+/, '')].reverse();
+  let greater = a.length > b.length ? a : b;
+  let product = [0];
+  greater.forEach((_, i) => {
+    let sum = eval(`${a[i] || 0} + ${b[i] || 0}`);
+    product.unshift(sum % 10);
+    product[1] += Number(sum > 9);
+  });
+  return product.reverse().join``;
 }
