fefadd56f735c49c49493986e59ddbe3bfd4d65b
Brooks Benson <brooksbenson01@gmail.com>
2018-03-14 10:02:28 -0600

    progress on permutations

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -1,7 +1,19 @@
 /*
-  Return all the possible permutations of a string
-  stored in an array.
+  A set or number of things can be ordered or arranged 
+  a finite number of ways. A permutation is one of the
+  possible arrangements of a set.
+
+  The permutations function defines a string as its
+  parameter and returns every unique permutation
+  of that string.
 */
 
 function permutations(s) {
+  return function build(set, perms) {
+    if (set.length === 0) return perms; 
+    set.forEach((c, i) => {
+      let root = !perms.length ? [c] : [...perms];
+      let newSet = [...set].splice(i, 1);
+    });
+  }([...s], []);
 }
--- permutations/permutations.test.js
+++ permutations/permutations.test.js
@@ -2,4 +2,4 @@ const permutations = require('./permutations');
 
 test('should return all the permutations of abc', () => {
   expect(permutations('abc')).toEqual(['abc', 'bac', 'bca', 'cba', 'cab', 'acb']);
-})
+});
